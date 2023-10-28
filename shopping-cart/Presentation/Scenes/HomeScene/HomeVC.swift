051002b3973bed53f8d742b48932077a01095af0
osmnnyldzz <osmanyildiz046@gmail.com>
2023-10-28 17:50:56 +0300

    update homeui

--- shopping-cart/Presentation/Scenes/HomeScene/HomeVC.swift
+++ shopping-cart/Presentation/Scenes/HomeScene/HomeVC.swift
@@ -10,8 +10,8 @@ import SnapKit
 
 final class HomeVC: UIViewController {
     
-    private var viewModel: HomeViewModel!
-    private var categories: Categories?
+    private var viewModel: HomeViewModel?
+    private var homeView = UIHomeView()
     
     init(viewModel: HomeViewModel) {
         self.viewModel = viewModel
@@ -21,77 +21,35 @@ final class HomeVC: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("")
     }
-    
-    private lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.backgroundColor = .clear
-        cv.isUserInteractionEnabled = true
-        cv.translatesAutoresizingMaskIntoConstraints = false
-        return cv
-    }()
 }
 
 extension HomeVC {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
-        view.addSubview(collectionView)
+        
         title = "Shopping Cart"
-
+        
+        homeView.delegate = self
+        view = homeView
+        
         self.bindData()
-        self.setupCollectionView()
-        self.setConstraints()
-     
-    }
-    
-    private func setupCollectionView(){
-        self.collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
-        self.collectionView.dataSource = self
-        self.collectionView.delegate = self
-    }
-    
-    private func setConstraints() {
-        // Base Collection View Constraints
-        self.collectionView.snp.makeConstraints { make in
-            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
-            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
-            make.leading.equalTo(self.view.snp.leading).offset(8.0)
-            make.trailing.equalTo(self.view.snp.trailing).offset(-8.0)
-          
-        }
     }
     
     private func bindData() {
-        self.viewModel.fetchCategories { response in
-            self.categories = response
-            self.collectionView.reloadData()
+        self.viewModel?.fetchCategories { [weak self] response in
+            guard let self = self else { return }
+            self.homeView.categories = response
+            self.homeView.collectionView.reloadData()
         }
     }
 }
 
-extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.categories?.categories.count ?? 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.identifier, for: indexPath) as! HomeCollectionViewCell
-        
-        cell.categoryTitleLabel.text = self.categories?.categories[indexPath.row]
-        cell.categoryBannerImageView.image = self.categories?.categoryImage[indexPath.row]
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: self.view.frame.width - 16, height: 200)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+extension HomeVC: UIHomeViewDelegate {
+    func categoryTapped(categoryName:String) {
         let vc = CategoryListViewController(viewModel: DIContainer.instance.categoryListViewModel())
         
-        self.viewModel.fetchAllSingleCategory(categoryName: self.categories?.categories[indexPath.row] ?? "") { response in
+        self.viewModel?.fetchAllSingleCategory(categoryName: categoryName) { [weak self] response in
+            guard let self = self else { return }
             vc.products = response
             self.navigationController?.pushViewController(vc, animated: true)
         }
---
+++ shopping-cart/Presentation/UIComponents/UIHomeView.swift
@@ -0,0 +1,83 @@
+//
+//  UIHomeView.swift
+//  shopping-cart
+//
+//  Created by Osman Yıldız on 28.10.2023.
+//
+
+import UIKit
+import SnapKit
+
+protocol UIHomeViewDelegate: AnyObject {
+    func categoryTapped(categoryName:String)
+}
+
+class UIHomeView: UIView {
+    
+    var categories: Categories?
+    weak var delegate: UIHomeViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .clear
+        cv.isUserInteractionEnabled = true
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+    
+    private func setupUI() {
+        backgroundColor = .white
+        addSubview(collectionView)
+        
+        self.setupCollectionView()
+        self.setConstraints()
+    }
+    
+    private func setupCollectionView() {
+        self.collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+    }
+    
+    private func setConstraints() {
+        self.collectionView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom)
+            make.leading.equalTo(snp.leading).offset(8.0)
+            make.trailing.equalTo(snp.trailing).offset(-8.0)
+        }
+    }
+}
+
+extension UIHomeView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.categories?.categories.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.identifier, for: indexPath) as! HomeCollectionViewCell
+        
+        cell.categoryTitleLabel.text = self.categories?.categories[indexPath.row]
+        cell.categoryBannerImageView.image = self.categories?.categoryImage[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width - 16, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        delegate?.categoryTapped(categoryName:self.categories?.categories[indexPath.row] ?? "")
+    }
+}
+
